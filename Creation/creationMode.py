b454fd563680d17dc8822e10b5d5f60838203251
alejandromonux <43568325+alejandromonux@users.noreply.github.com>
2020-03-21 19:13:57 +0100

    Update creationMode.py

--- Creation/creationMode.py
+++ Creation/creationMode.py
@@ -6,7 +6,7 @@ from Json import *
 
 
 def create_char():
-    isRight = 1
+    isRight = 0
     #TODO Crear nuevo pesonaje (new Character) e ir llenando
     with open('characterTemplate.json', 'r') as myfile:
         data = myfile.read()
@@ -20,48 +20,48 @@ def create_char():
     print("Interesting! What about your class?\n")
     Class = input()
     chara.character_class = Class
-    while isRight==1:
+    while isRight==0:
         print("So, what about your stats?\n"
               "a)Roll them now!\n"
               "b)Let me set those\n")
         choice = input()
         if choice == "a":
-            isright = 1
-            chara.stats = SavingThrow( random.randrange(3,18),
+            isRight = 1
+            chara.stats = Stats( random.randrange(3,18),
                                        random.randrange(3,18),
                                        random.randrange(3,18),
                                        random.randrange(3,18),
                                        random.randrange(3,18),
                                        random.randrange(3,18))
-            chara.saving_throws = SavingThrow((chara.stats.strength-10)/2,
-                                              (chara.stats.dexterity - 10) / 2,
-                                              (chara.stats.constitution - 10) / 2,
-                                              (chara.stats.intelligence - 10) / 2,
-                                              (chara.stats.wisdom - 10) / 2,
-                                              (chara.stats.charisma - 10) / 2)
+            
+            chara.saving_throws = Stats(int((chara.stats.strength-10)/2),
+                                        int((chara.stats.dexterity - 10) / 2),
+                                        int((chara.stats.constitution - 10) / 2),
+                                        int((chara.stats.intelligence - 10) / 2),
+                                        int((chara.stats.wisdom - 10) / 2),
+                                        int((chara.stats.charisma - 10) / 2))
 
             pass
         elif choice == "b":
+            isRight = 1
             print("Strength:")
-            chara.stats[0] = input()
+            chara.stats.strength = int(input())
             print("Dexterity:")
-            chara.stats[1] = input()
+            chara.stats.dexterity = int(input())
             print("Constitution:")
-            chara.stats[2] = input()
+            chara.stats.constitution = int(input())
             print("Intelligence:")
-            chara.stats[3] = input()
+            chara.stats.intelligence = int(input())
             print("Wisdom:")
-            chara.stats[4] = input()
+            chara.stats.wisdom = int(input())
             print("Charisma")
-            chara.stats[5] = input()
-            chara.saving_throws = SavingThrow((chara.stats[0]-10)/2,
-                                              (chara.stats[1] - 10) / 2,
-                                              (chara.stats[2] - 10) / 2,
-                                              (chara.stats[3] - 10) / 2,
-                                              (chara.stats[4] - 10) / 2,
-                                              (chara.stats[5] - 10) / 2)
-
-
+            chara.stats.charisma = int(input())
+            chara.saving_throws = Stats(int((chara.stats.strength-10)/2),
+                                        int((chara.stats.dexterity - 10) / 2),
+                                        int((chara.stats.constitution - 10) / 2),
+                                        int((chara.stats.intelligence - 10) / 2),
+                                        int((chara.stats.wisdom - 10) / 2),
+                                        int((chara.stats.charisma - 10) / 2))
 
             isright = 1
             pass
@@ -69,18 +69,19 @@ def create_char():
             isright = 0
             print("That's not a valid choice, my friend!\n")
 
-    isRight = 1
+    isRight = 0
     #Comprobar que level sigui vàlid
-    while  isRight==1:
+    while  isRight==0:
         print("At what level should we start?\n")
-        level = input()
+        level = int(input())
+        isRight = 1
         chara.level = level
-        if  (level <= 20) or (level >= 1):
+        if  (level >= 20) or (level <= 1):
             print("That's not a valid level, my friend!\n")
             isRight = 0
 
-    isRight = 1
-    while isRight == 1:
+    isRight = 0
+    while isRight == 0:
         print("Well then, it's time to choose your background now!\n"
               "a)Scholar!\n"
               "b)Slave\n"
@@ -92,23 +93,29 @@ def create_char():
         else:
             isRight = 0
             print("That's not a valid choice, my friend!\n")
-    isRight = 1;
-    while isRight == 1:
+
+    isRight = 0
+    while isRight == 0:
         print("Do you want to input a description? Y/N\n")
-        answer = input();
+        answer = input()
         if answer == "Y":
+            isRight = 1
             print("Let's see it!\n")
             description = input()
             chara.description = description
             pass
         elif answer == "N":
+            isRight = 1
             print("That's fine, too!")
             pass
         else:
             isRight=0
             print("Not a valid answer, friend!\n")
 
+    #TODO: Escribir el nuevo PJ en el JSON
     with open('chara.json', 'r') as myfile:
         data = myfile.read()
     characters = characters_from_dict(json.loads(data))
     characters.append(chara)
+    #characters_to_dict(characters)
+    json.dump(characters)
