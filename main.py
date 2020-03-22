4370115c34647467a1bb279f8037340b30c40a2c
alejandromonux <43568325+alejandromonux@users.noreply.github.com>
2020-03-22 18:37:18 +0100

    Feat: Main Looped

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ from Gestion.gestionMode import gestion_mode
 
 print("Hello, I'm MattBot.")
 print("What do you need for today's game?")
-print(" - Character creation")
-print(" - Character gestion")
-print(" - Companion mode")
-mode = input()
-result = re.match(mode, "create|create", re.IGNORECASE)
-if result:
-    create_char()
+while True:
+    print(" - Character creation")
+    print(" - Character gestion")
+    print(" - Companion mode")
+    mode = input()
+    result = re.match(mode, "create|create", re.IGNORECASE)
+    if result:
+        create_char()
 
-result = re.match(mode, "gestion", re.IGNORECASE)
-if result:
-    gestion_mode()
+    result = re.match(mode, "gestion", re.IGNORECASE)
+    if result:
+        gestion_mode()
 
-result = re.match(mode, "companion", re.IGNORECASE)
-if result:
-    companion_mode()
+    result = re.match(mode, "companion", re.IGNORECASE)
+    if result:
+        companion_mode()
+    print("\nAnything else?")
