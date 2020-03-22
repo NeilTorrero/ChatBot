82db03cd2d94ec5261fb4de3d29d5ad6d0972bc6
NeilTorrero <neil.torrero@students.salle.url.edu>
2020-03-22 11:05:05 +0100

    Rolling dice companion

--- Companion/companionMode.py
+++ Companion/companionMode.py
@@ -28,7 +28,18 @@ def use_spell_feat():
 
 
 def dice_roll():
-    print("Dice")
+    print("What kind of roll you want to do?")
+    kind = input()
+    result = re.match(kind, "attack|skill|dc",  re.IGNORECASE)
+    if result:
+        result = re.match(kind, "attack", re.IGNORECASE)
+        if result:
+            print("roll attack")
+        result = re.match(kind, "skill|dc", re.IGNORECASE)
+        if result:
+            print("roll skill")
+    else:
+        print("Sorry but I don't have that rolling option.")
 
 
 def companion_mode():
@@ -39,20 +50,29 @@ def companion_mode():
     # JSON get characters
     [theres_character, found_character] = character_selection(characters)
     print(found_character.name)
+    print("What do you need?")
+    print("Use a spell or feature?")
+    print("Do dice rolls?")
+    print("Or maybe do some of the other functionalities?")
     if theres_character:
         back = True
         while back:
             need = input()
-            result = re.match(need, "dice", re.IGNORECASE)
+            result = re.match(need, "dice|feature|spell|back|return|exit", re.IGNORECASE)
             if result:
-                dice_roll()
+                result = re.match(need, "dice", re.IGNORECASE)
+                if result:
+                    dice_roll()
 
-            result = re.match(need, "feature|spell", re.IGNORECASE)
-            if result:
-                use_spell_feat()
+                result = re.match(need, "feature|spell", re.IGNORECASE)
+                if result:
+                    use_spell_feat()
 
-            result = re.match(need, "back|return|exit", re.IGNORECASE)
-            if result:
-                back = False
+                result = re.match(need, "back|return|exit", re.IGNORECASE)
+                if result:
+                    back = False
+            else:
+                print("Sorry but I don't have that option.")
+                print("Please choose one of the options I have offered to you.")
     else:
         create_char()
