21dc8298ad34ea735f7b0c662f0e29988897db5e
Neil <neil.torrero@students.salle.url.edu>
2020-05-08 20:37:40 +0200

    feat: Calls to dialogflow API

--- API.py
+++ API.py
@@ -1,6 +1,14 @@
 import requests
 
 BASE_URL = 'http://www.dnd5eapi.co'
+"""
+DIALOGFLOW_PROJECT_ID = 'd-d-otyuot'
+DIALOGFLOW_LANGUAGE_CODE = 'en-US'
+GOOGLE_APPLICATION_CREDENTIALS = 'd-d-otyuot-b3f7caf37164.json'
+SESSION_ID = '123456789'
+
+add to enviroment variables the ggogle_application_credentials
+"""
 
 def getInfoAPI(type, name):
     url = BASE_URL + '/api/' + type + '/' + name + '/'
@@ -12,3 +20,28 @@ def getInfoAPI(type, name):
     print(data['range'])
     print(data['desc'])
     return data
+
+
+def dialogflow():
+    import dialogflow_v2 as dialogflow
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path("d-d-otyuot", "123456789")
+    print('Session path: {}\n'.format(session))
+
+    text_input = dialogflow.types.TextInput(
+        text="I want to create a character", language_code="en-US")
+
+    query_input = dialogflow.types.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        session=session, query_input=query_input)
+
+    print('=' * 20)
+    print('Query text: {}'.format(response.query_result.query_text))
+    print('Detected intent: {} (confidence: {})\n'.format(
+        response.query_result.intent.display_name,
+        response.query_result.intent_detection_confidence))
+    print('Fulfillment text: {}\n'.format(
+        response.query_result.fulfillment_text))
+    print(response)
