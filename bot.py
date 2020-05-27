a0d0570ae4ad30609eb1c237a4279803047a633e
Neil <neil.torrero@students.salle.url.edu>
2020-05-27 13:18:32 +0200

    chore: Python reformat

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
-from telegram.ext import Updater,CommandHandler,MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from API import dialogflow
 
-updater = Updater(token='1228506430:AAHhakTQS0moSjszpVzXC8yyXXHMqJ195OY', use_context=True)# Telegram API Token
+updater = Updater(token='1228506430:AAHhakTQS0moSjszpVzXC8yyXXHMqJ195OY', use_context=True)  # Telegram API Token
 dispatcher = updater.dispatcher
 
 import logging
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                    level=logging.INFO)
+
 
 def infoTreatment(response, username):
     print(response)
@@ -15,14 +17,16 @@ def infoTreatment(response, username):
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
 def textMessage(update, context):
-    #Sends user input to dialogflow and recieves response
+    # Sends user input to dialogflow and recieves response
     response = dialogflow(update.message.text, update.message.chat_id)
-    #Call to python database gestion
+    # Call to python database gestion
     infoTreatment(response, update.message.chat.username)
-    #Telegram bot writes response of dialogflow
+    # Telegram bot writes response of dialogflow
     context.bot.send_message(chat_id=update.message.chat_id, text=response.query_result.fulfillment_text)
-    #context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    # context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
 start_handler = CommandHandler('start', start)
@@ -31,4 +35,4 @@ updater.start_polling()
 text_handler = MessageHandler(Filters.text & (~Filters.command), textMessage)
 dispatcher.add_handler(text_handler)
 updater.idle()
-#updater.stop()
+# updater.stop()
