28c35347a3f46dbe17832f390c6cd189d073825f
Neil <neil.torrero@students.salle.url.edu>
2020-05-30 16:05:30 +0200

    fix: Clean up

--- tools/Skills&ST.py
+++ tools/Skills&ST.py
@@ -21,19 +21,13 @@ def skillsAndSTCreation(stats, saving_throws, skills, className, proficiencyBonu
         "survival": "wisdom"
     }
 
-    ST2stat = {
-        "STR" : "strength",
-        "DEX" : "dexterity"
-    }
-
     for skill in list(skill2Stat.keys()):
         skills[skill] = (stats[skill2Stat[skill]] - 10) / 2
-
 
     data = getInfoAPI("classes", className)
 
     for st in list(saving_throws.keys()):
         if data["saving_throws"][0]["name"][:3].lower() == st[:3].lower() \
-            or data["saving_throws"][1]["name"][:3].lower() == st[:3]:
-            saving_throws[st] = ((stats[st] - 10)/2) + proficiencyBonus
+                or data["saving_throws"][1]["name"][:3].lower() == st[:3]:
+            saving_throws[st] = ((stats[st] - 10) / 2) + proficiencyBonus
         saving_throws[st] = (stats[st] - 10) / 2
